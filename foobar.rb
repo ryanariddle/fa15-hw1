2157076b08b381df9170f106f8e47f60fa4cbb06
ryanriddle@berkeley.edu <ryanriddle@berkeley.edu>
2015-09-25 14:55:17 -0700

    all test pass

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,12 @@
 class Foobar
-  # Q4 CODE HERE
+  def self.baz array
+  	a = Array.new(array)
+  	a = a.map { |e| e.to_i }
+  	a = a.map { |e| e+2 }
+  	a = a.keep_if { |e| e%2 == 0}
+  	a = a.keep_if { |c| c <= 10}
+  	sum = 0
+  	a.each { |e| sum += e }
+  	return sum
+  end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,17 +1,18 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  ss = a + b
+  ss = ss**2
+  return ss
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+  return a.sort.map { |e| e+1 }
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  return first_name + " " + last_name
 end
 
 def blockin_time(a)
-  # DO NOT EDIT THIS CODE BELOW
   require './foobar'
   Foobar.baz a
 end
